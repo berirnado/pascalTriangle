8d8dba1ec8e5f66cf3bd89ea0c5c0de7ae26ff12
Bernardo <be.zito@hotmail.com>
2024-12-14 18:34:58 -0300

    Teste com printf

--- feitoEmCasa/main.c
+++ feitoEmCasa/main.c
@@ -1,5 +1,29 @@
 #include <stdio.h>
 
-void main(){
-    printf("Hello World!");
+void generate(int linhas) {
+    int triangulo[linhas][linhas];
+
+    int i, j;
+    for ( i = 0; i < linhas; i++ ) {
+        for ( j = 0; j <= i; j++ ) {
+            if ( j == 0 || j == i ) {
+                triangulo[i][j] = 1;
+            } else {
+                triangulo[i][j] = triangulo[i - 1][j - 1] + triangulo[i - 1][j];
+            }
+            printf("%d ", triangulo[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+int main() {
+    int linhas;
+
+    printf("Digite o número de linhas para o Triângulo de Pascal: ");
+    scanf("%d", &linhas);
+
+    generate(linhas);
+
+    return 0;
 }
